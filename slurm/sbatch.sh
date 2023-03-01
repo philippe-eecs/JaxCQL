1c1803866ea38bb41ce02b1ddb532efad929c3bc
philippe-eecs <hansenpmeche@berkeley.edu>
2023-03-01 14:51:31 -0800

    new

--- slurm/sbatch.sh
+++ slurm/sbatch.sh
@@ -6,7 +6,7 @@
 #SBATCH --time=24:00:00
 #SBATCH --mem=24G
 #SBATCH --cpus-per-task=4
-#SBATCH --gres=gpu"1
+#SBATCH --gres=gpu:1
 #SBATCH --account=co_rail
 #SBATCH --partition=savio2_gpu
 #SBATCH --qos=savio_lowprio
